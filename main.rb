37b5fe67deb228676bf0410d4419d5c0cadc5748
Krister <krister@Kristers-MBP.lan>
2015-08-20 14:24:33 +0300

    Finalising the game

--- main.rb
+++ main.rb
@@ -2,14 +2,13 @@ require 'rubygems'
 require 'sinatra'
 require 'pry'
 
-# use Rack::Session::Cookie, :key => 'rack.session',
-#                            :path => '/',
-#                            :secret => '12345' 
-
-set :sessions, true
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => '12345' 
 
 helpers do
   BLACKJACK = 21
+  DEALER_MIN = 17
   CARD_VALUES = {'2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10, 'A' =>11}
   
   def total(cards)
@@ -48,15 +47,51 @@ helpers do
     value = find_value(card[1])
     "#{suit}_#{value}.jpg"
   end
+
+  def blackjack?(card)
+    total(card) == BLACKJACK and card.size == 2
+  end
+
+  def announce_winner(player_score, dealer_score)
+    if dealer_score <= BLACKJACK && (dealer_score > player_score || player_score > BLACKJACK)
+      "Dealer Wins"
+    elsif player_score <= BLACKJACK && (player_score > dealer_score || dealer_score > BLACKJACK)
+      "#{session[:player_name]} wins!"
+    else
+      "It's a tie"
+    end
+  end
+
+  def check_winner_calculate_balance(player_hand, dealer_hand)
+    if blackjack?(player_hand) && !blackjack?(dealer_hand)
+      session[:balance] += session[:bet] + session[:bet] * 1.5
+      @win = "#{session[:player_name]} wins with BLACKJACK."
+    elsif !blackjack?(player_hand) && blackjack?(dealer_hand)
+      @lost = "Dealer wins with BLACKJACK."
+    elsif total(player_hand) > total(dealer_hand) || (total(player_hand) <= BLACKJACK && total(dealer_hand) > BLACKJACK)
+      session[:balance] += session[:bet] * 2
+      @win = "Player wins!"
+    elsif total(player_hand) < total(dealer_hand) || (total(player_hand) > BLACKJACK && total(dealer_hand) <= BLACKJACK)
+      @lost = "Dealer wins!"
+    else
+      @win = "It's a tie!"
+      session[:balance] += session[:bet]
+    end
+    session[:game_started] = false
+  end
+
 end
 
 get '/' do
+  session[:game_started] = false
+  session[:player_turn] = true
   erb :index  
 end
 
 post '/user' do
   if params[:player_name].empty?
-    redirect '/'
+    @error = 'Player name is required!'
+    halt erb(:index)
   else
     session[:player_name] = params[:player_name]
     session[:balance] = 500
@@ -65,39 +100,47 @@ post '/user' do
 end
 
 get '/bet' do
-  if session[:player_name].empty?
-    redirect '/'
-  else
-    erb :bet
-  end
+  erb :bet
 end
 
 post '/bet' do
   if params[:bet].empty?
-    redirect '/bet'
+    @error = 'Bet is required'
+    erb :bet
   else
     session[:balance] -= params[:bet].to_i
     session[:bet] = params[:bet].to_i
+    session[:game_over] = false
     redirect '/play'
   end
 end
 
-post '/hit' do
-  session[:dealer_cards] << session[:deck].pop
-  erb :play
+post '/game/*/*' do |participant, action|
+  if participant == 'player'
+    if action == 'hit'
+      session[:player_cards] << session[:deck].pop
+      @info = 'Player decided to hit!'
+    elsif action == 'stay'
+      @info = "Player decided to stay! Player has #{total(session[:player_cards])}"
+      session[:player_turn] = false
+    end
+  elsif participant == 'dealer'
+    if action == 'hit'
+      session[:dealer_cards] << session[:deck].pop
+    end
+  end
+  redirect '/play'
 end   
 
 get '/play' do
-  
-  session[:player_turn] = 1
-  if session[:player_name].empty?
-    redirect '/'
-  elsif session[:bet] == 0
-    redirect '/bet'
-  else
+  if !session[:game_started]
     SUITS = ['H','D','C','S']
     CARDS = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
     session[:deck] = SUITS.product(CARDS).shuffle!
+
+    session[:game_started] = true
+    session[:player_turn] = true
+    session[:game_over] = false
 
     session[:player_cards] = []
     session[:dealer_cards] = []
@@ -105,6 +148,26 @@ get '/play' do
     session[:dealer_cards] << session[:deck].pop
     session[:player_cards] << session[:deck].pop
     session[:dealer_cards] << session[:deck].pop
-    erb :play
+
+    # session[:player_cards] = [['H','A'],['H','K']]
   end
+  
+  if blackjack?(session[:player_cards]) 
+    session[:player_turn] = false
+    @info = "#{session[:player_name]} got BLACKJACK. Checking dealers card" 
+  elsif total(session[:player_cards]) > BLACKJACK
+    session[:player_turn] = false
+    @info = "#{session[:player_name]} busted!"
+  end
+
+  if !session[:player_turn] && total(session[:dealer_cards]) >= DEALER_MIN
+    session[:game_over] = true
+    check_winner_calculate_balance(session[:player_cards], session[:dealer_cards])
+  end
+  erb :play
+end
+
+
+get '/exit' do
+  erb :exit
 end
